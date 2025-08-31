e2fca670d9e0724c866940fc3567863690a98623
gurayerus <gurayerus@gmail.com>
2025-08-31 18:54:20 -0400

    Edits

--- test/scripts/testrun_ravens_ants.sh
+++ test/scripts/testrun_ravens_ants.sh
@@ -15,7 +15,7 @@ outdir="$(cd ../output && pwd)"
 tImg=${tdir}/colin27_t1_tal_lin_T1_LPS_dlicv.nii.gz
 
 mrid='subj1'
-# mrid='subj2'
+mrid='subj2'
 
 t1=${indir}/${mrid}/${mrid}_T1_LPS_dlicv.nii.gz
 t1seg=${indir}/${mrid}/${mrid}_T1_LPS_dlicv_seg.nii.gz
